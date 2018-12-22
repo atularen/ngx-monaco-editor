4d98f9de790a9cc8fe9027c385486c74b99e605d
Sushil Pandey <sushilpandey@Sushils-MacBook-Air.local>
2018-12-23 02:32:51 +0530

    Added Electron support

---
+++ electron.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 1200, height: 800,  webPreferences: { nodeIntegration: false } })
+
+  // load the dist folder from Angular
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // Open the DevTools optionally:
+  // win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
