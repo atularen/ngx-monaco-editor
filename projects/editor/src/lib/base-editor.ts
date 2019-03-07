4a2ae8e4456aa67bcb7442a05854e167b6fbf883
Remo Loetscher <remo.loetscher@bison-group.com>
2019-03-07 12:58:04 +0100

    add relativ default path for loading monaco-editor

--- projects/editor/src/lib/base-editor.ts
+++ projects/editor/src/lib/base-editor.ts
@@ -37,7 +37,7 @@ export abstract class BaseEditor implements AfterViewInit, OnDestroy {
     } else {
       loadedMonaco = true;
       loadPromise = new Promise<void>((resolve: any) => {
-        const baseUrl = this.config.baseUrl || '/assets';
+        const baseUrl = this.config.baseUrl || './assets';
         if (typeof ((<any>window).monaco) === 'object') {
           resolve();
           return;
